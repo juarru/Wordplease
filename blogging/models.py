6dd9df34e3ea6c08a9981a4b3610a58d29c2f1f4
Juan Arillo <juanarillo@outlook.com>
2018-01-03 16:25:44 +0100

    Added Category and Post model. Added Category to admin site

--- blogging/models.py
+++ blogging/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        """
+        Returns a string that identifies the model
+        :return: String
+        """
+
+        return self.name
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=100)
+    summary = models.CharField(max_length=250)
+    post = models.TextField()
+    image = models.ImageField(null=True)
+    url = models.URLField(null=True)
+    release_date = models.DateTimeField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """
+        Returns a string that identifies the model
+        :return: String
+        """
+
+        return self.title
